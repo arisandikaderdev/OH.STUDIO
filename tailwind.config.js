6c539aa188ba1c5010095e3147302fa1be9a0213
Arisandi Kader <arisandikaderdev@gmail.com>
2023-08-14 11:18:51 +0900

    design layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ["open sans", "sans-serif"],
+    },
     extend: {
       colors: {
         primary_white: "#fff",
         primary_grey: "#7f7f7f",
+        primary_white_alpha: "rgba(0,0,0,.1)",
       },
     },
   },
